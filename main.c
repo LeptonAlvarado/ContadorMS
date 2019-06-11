68403e1301881d336e038a5b3bb637f7e8b4f8e4
LeptonAlvarado <48077048+LeptonAlvarado@users.noreply.github.com>
2019-06-11 12:59:58 -0500

    Codigo Base
    Base para empezar el codigo

---
+++ main.c
@@ -0,0 +1,43 @@
+#include<18LF4620.h>
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES NOFCMEN                  //Fail-safe clock monitor disabled
+#FUSES NOIESO                   //Internal External Switch Over mode disabled
+#FUSES PUT                      //Power Up Timer
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOPBADEN                 //PORTB pins are configured as digital I/O on RESET
+#FUSES NOLPT1OSC                //Timer1 configured for higher power operation
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOXINST                  //Extended set extension and Indexed Addressing mode disabled (Legacy mode)
+
+//#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
+//#use fast_io(a)
+
+#int_timer0
+int countTimer;
+void isr_timer0()
+{
+    //countTimer++;
+    set_timer0();
+}
+
+#int_RDA
+void isr_serial()
+{
+}
+
+#use delay(clock=16000000)
+
+void main()
+{
+    setup_oscillator(OSC_16MHZ);
+    setup_adc_ports(NO_ANALOGS);
+    setup_timer_0(RTCC_INTERNAL|RTCC_DIV_8);
+    set_timer0(15536);
+    enable_interrupts(INT_TIMER0);
+    enable_interrupts(INT_RDA);
+    enable_interrupts(GLOBAL);
+    while(TRUE)
+    {
+       
+    }
+}
